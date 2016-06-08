28fa8f26f65853b6955fd1959c04b014dcb5c992
kian1213 <chrono0424@gmail.com>
2016-06-08 11:23:36 +0800

    initialize connect 4 game

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root "home#index"
   get  "rps/index", as: "rps"
+  get  "connect/index", as: "connect"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
